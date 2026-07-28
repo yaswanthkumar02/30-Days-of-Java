627ba4c7421f8e1a94a548fa48d2cea10ffd8281
Yaswanth kumar <yaswanth0913@gmail.com>
2026-07-28 13:21:10 +0530

    Add Day 3 - Largest of Three Numbers

---
+++ Day-3 Largest-of-three-numbers/Largestofthreenums.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Largestofthreenums {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int y = sc.nextInt();
+
+        System.out.print("Enter the third number : ");
+        int z = sc.nextInt();
+
+        if (x > y && x > z ) {
+            System.out.println(x + " is the largest.");} 
+        else if(y > z && y > x) {
+            System.out.println(y + " is the largest");
+        } else 
+            System.out.println(z + " is the largest");
+
+            sc.close();
+    }
+}
