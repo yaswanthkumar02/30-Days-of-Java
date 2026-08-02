0aa885e81b09be797c37051d55cbcf027aa6a87b
Yaswanth kumar <yaswanth0913@gmail.com>
2026-08-02 13:00:35 +0530

    Create Reversenumber.java

---
+++ Day-08 Reverse Number/Reversenumber.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Reversenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int reverse = 0;
+
+       while(n != 0){
+           int digit = n % 10;
+            reverse = reverse * 10 + digit ;
+            n = n / 10;
+        }
+        System.out.println("Reversed Number: " + reverse);
+        sc.close();
+    }
+}
+
+
